de0d66aa789df7d2ae19fbb888d4bc3346914290
tk3fftk <tktk.stereoman@gmail.com>
2021-02-04 19:17:18 +0900

    6. draw

--- src/index.js
+++ src/index.js
@@ -120,6 +120,8 @@ class Game extends React.Component {
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
+    } else if (history.length === 10) {
+      status = 'Draw';
     } else {
       status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
